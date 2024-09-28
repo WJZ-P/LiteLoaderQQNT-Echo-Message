7b881aeef5c62252dad22a5785f33e313d09ed8a
WJZ_P <110795301+WJZ-P@users.noreply.github.com>
2024-09-29 01:11:44 +0800

    随便写写

--- src/utils/rendererUtils.js
+++ src/utils/rendererUtils.js
@@ -1,9 +1,15 @@
 const pluginName = '[Echo-Message]'
 
 export async function messageRenderer(allChats) {
+
     for (let i = 0; i < allChats.length; i++) {
         try {
-            const msgContentContainer = allChats[i + 1]?.querySelector('.msg-content-container')
+            const msgContentContainer = allChats[i]?.querySelector('.msg-content-container')
+
+            const preMsgConContainer = i > 0 ? allChats[i - 1]?.querySelector('.msg-content-container') : null;
+
+            if (preMsgConContainer?.querySelector('.em-svg-container')) removePlusOneTag(msgContentContainer)//只保留一个svg
+
             if (msgContentContainer?.classList.contains('em-msg')) continue//已经改过的不要改
 
             const currentMsgContent = allChats[i]?.querySelector('.message-content');
@@ -15,7 +21,7 @@ export async function messageRenderer(allChats) {
             if (!msgChecker(prevMsgContent, currentMsgContent, nextMsgContent)) continue
 
             //没问题！应该对下一条消息加上+1标签。
-            console.log(pluginName+'消息检查成功')
+            console.log(pluginName + '消息检查成功')
             appendPlusOneTag(msgContentContainer)//添加tag
 
         } catch (e) {
@@ -24,24 +30,6 @@ export async function messageRenderer(allChats) {
     }
 }
 
-
-function appendPlusOneTag(msgContentContainer) {
-    const imgElement = document.createElement('img')
-    imgElement.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#66ccff">
-<path d="M250-292.31v-120H130v-60h120v-120h60v120h120v60H310v120h-60Zm391.54 71.54v-428.77l-96.62 68.31-34.46-51.54 149.39-106.46h47.84v518.46h-66.15Z"/>
-</svg>`
-    msgContentContainer.classList.add('em-msg')//先修改父元素样式
-
-    if (msgContentContainer?.classList.contains('container--others'))//说明是别人发的消息
-    {
-        imgElement.classList.add('em-plus-one-img-right')
-    } else {
-        imgElement.classList.add('em-plus-one-img-left')
-    }
-
-    msgContentContainer.appendChild(imgElement)
-    console.log(pluginName+'tag添加成功')
-}
 
 /**
  * 检查当前元素是否和上一个相同，同时和下一个不同
@@ -54,8 +42,8 @@ function msgChecker(prevMsgContent, currentMsgContent, nextMsgContent) {
     const currentMsgs = msgExtractor(currentMsgContent)
     const nextMsgs = msgExtractor(nextMsgContent)
     //console.log(JSON.stringify(prevMsgs), JSON.stringify(currentMsgs), JSON.stringify(nextMsgs))
-    return JSON.stringify(prevMsgs) === JSON.stringify(currentMsgs) &&
-        JSON.stringify(currentMsgs) !== JSON.stringify(nextMsgs)
+    return JSON.stringify(nextMsgs) === JSON.stringify(currentMsgs) &&
+        JSON.stringify(currentMsgs) !== JSON.stringify(prevMsgs)
 }
 
 /**
@@ -67,6 +55,56 @@ function msgExtractor(msgContent) {
     if (!msgContent?.querySelectorAll) return []
     return [...(Array.from(msgContent?.querySelectorAll('.text-normal')).map(textElement => textElement?.innerText)),
         ...(Array.from(msgContent?.querySelectorAll('.image-content')).map(imgElement => imgElement?.src))]
+}
+
+/**
+ * 添加+1tag
+ * @param msgContentContainer
+ */
+function appendPlusOneTag(msgContentContainer) {
+    const svgContainer = document.createElement('div');
+    svgContainer.className = 'em-svg-container'
+    svgContainer.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#66ccff"><path d="M250-292.31v-120H130v-60h120v-120h60v120h120v60H310v120h-60Zm391.54 71.54v-428.77l-96.62 68.31-34.46-51.54 149.39-106.46h47.84v518.46h-66.15Z"/></svg>`
+    svgContainer.style.display = 'flex';
+    svgContainer.style.justifyContent = 'center'; // 水平居中
+    svgContainer.style.alignItems = 'center'; // 垂直居中
+    svgContainer.style.cursor = 'pointer'; // 鼠标悬停时光标变为手指
+
+    msgContentContainer.classList.add('em-msg')//先修改父元素样式
+    msgContentContainer.appendChild(svgContainer)
+
+    if (msgContentContainer?.classList.contains('container--others'))//说明是别人发的消息
+    {
+        svgContainer.classList.add('em-plus-one-img-right')
+    } else {
+        svgContainer.classList.add('em-plus-one-img-left')
+    }
+
+    setTimeout(() => {
+        svgContainer.style.transform = msgContentContainer?.classList.contains('container--others') ?
+            "translateX(50%)" : "translateX(-50%)";
+        svgContainer.style.opacity = "0.9";
+        svgContainer.style.border = "2px solid #66ccff";
+    }, 100);
+    console.log(pluginName + '+1tag添加成功')
+}
+
+
+function removePlusOneTag(msgContentContainer) {
+    const svgContainer = msgContentContainer?.querySelector('.em-svg-container')
+
+    svgContainer.style.opacity = '0'
+    if (msgContentContainer?.classList.contains('container--others'))//说明是别人发的消息
+    {
+        svgContainer.style.transform = "translateX(-100%)"
+    } else {
+        svgContainer.style.transform = "translateX(100%)"
+    }
+
+    setTimeout(() => {
+        msgContentContainer.removeChild(svgContainer)//移除掉多余的svg
+    }, 500)
+
 }
 
 export function patchCss() {
@@ -82,34 +120,42 @@ export function patchCss() {
     overflow: unset !important;
 }
 
+.em-svg-container:hover {
+    opacity: 1; /* 鼠标悬停时完全不透明 */
+    transform: scale(1.1); /* 放大效果 */
+}
+
+.em-svg-container:active {
+    transform: scale(0.95); /* 点击时缩小效果 */
+}
+
 .em-plus-one-img-right {
     position: absolute;
-    left: calc(100% + 6px);
-    top:50%;
-    width: 30px;
-    height: 30px;
+    left: calc(95%);
+    width: 25px;
+    height: 25px;
     border-radius: 50%;
-    opacity: 0.8;
+    opacity: 0.2;
     color: var(--text-color);
     background-color: var(--background-color-05);
-    backdrop-filter: blur(28px);
+    backdrop-filter: blur(14px);
     box-shadow: var(--box-shadow);
-    transition: 300ms;
+    transition: 250ms;
 }
+
 
 .em-plus-one-img-left {
     position: absolute;
-    right: calc(100% + 6px);
-    top:50%;
-    width: 30px;
-    height: 30px;
+    right: calc(95%);
+    width: 25px;
+    height: 25px;
     border-radius: 50%;
-    opacity: 0.8;
+    opacity: 0.2;
     color: var(--text-color);
     background-color: var(--background-color-05);
     backdrop-filter: blur(28px);
     box-shadow: var(--box-shadow);
-    transition: 300ms;
+    transition: 250ms;
 }
 
 `
