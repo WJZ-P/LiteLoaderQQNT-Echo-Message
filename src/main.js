5d92e88f663cee2341cb999a7098ebf6a1e21335
WJZ_P <110795301+WJZ-P@users.noreply.github.com>
2024-10-04 14:59:36 +0800

    小修小布辣

--- src/main.js
+++ src/main.js
@@ -5,19 +5,7 @@ const {pluginLog} = require("./utils/backendLogUtils.js");
 exports.onBrowserWindowCreated = (window) => {
     try {
         // window 为 Electron 的 BrowserWindow 实例
-        window.webContents.on("did-stop-loading", async () => {
-            if (window.webContents.getURL().indexOf("#/main/message") === -1 &&
-                window.webContents.getURL().indexOf("#/chat") === -1
-            ) {
-                pluginLog('当前窗口ID为' + window.id)
-                pluginLog(window.webContents.getURL())
-                pluginLog(window.webContents.getURL().indexOf("#/main/message"))
-                return;
-            }
-            // 进入聊天页面时触发渲染
-            pluginLog('检测到聊天页面,通知渲染进程')
-            window.echo_message.startRender()//给渲染进程发消息，说明当前是聊天窗口，可以进行渲染。
-        })
+
     } catch (e) {
         pluginLog(e)
     }
